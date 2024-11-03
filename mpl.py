cbd260c62de72457c3b4e9b664e8d33591e5ac26
babilon15 <babylon177@proton.me>
2024-11-03 08:13:48 +0100

    1103

--- mpl.py
+++ mpl.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-# pylint:disable=line-too-long,too-many-statements,too-many-branches,too-many-instance-attributes
+# pylint:disable=line-too-long, missing-function-docstring, missing-class-docstring
 
 """Simple and easy-to-use text-based program for launching mpv."""
 
+from signal import signal, SIGCHLD, SIG_IGN, SIGTERM
 from string import digits, punctuation, whitespace
-from signal import signal, SIGCHLD, SIG_IGN
 from subprocess import Popen, DEVNULL
 from collections.abc import Sequence
 from dataclasses import dataclass
 from pathlib import Path
 from shlex import split
 import curses
+import sys
 import os
 
 from natsort import natsorted
@@ -33,6 +34,14 @@ MPV_EXTENSIONS: tuple[str, ...] = (
 
 ENV_MPVL_MPV_CMD: str = os.getenv("MPVL_MPV_CMD", "mpv --force-window")
 MPV_SUB_FILE_OPTION: str = "--sub-file="
+MPV_PAUSE_FLAG: str = "--pause"
+MPV_DVD_ISO_OPTION: str = "dvd:// --dvd-device="
+MPV_BD_ISO_OPTION: str = "bd:// --bluray-device="
+
+# ISO approx.:
+# SMALLER -> DVD
+# BIGGER  -> BD
+DVD9_SIZE: int = 8500000000
 
 IGNORED_NAMES: tuple[str, ...] = ("lost+found", ".git")  # Case-insensitive!
 
@@ -45,12 +54,29 @@ def exec_nonblocking(cmd: str) -> int:
     return p.pid
 
 
-def mpv_open(path: str, sub_path: str = ""):
+def mpv_open(path: str, sub_path: str = "", paused: bool = False) -> int:
     cmd: list[str] = [ENV_MPVL_MPV_CMD]
+    if paused:
+        cmd.append(MPV_PAUSE_FLAG)
     if sub_path:
         cmd.append("".join((MPV_SUB_FILE_OPTION, '"', sub_path, '"')))
     cmd.append("".join(('"', path, '"')))
     return exec_nonblocking(SPACE_CHAR.join(cmd))
+
+
+def mpv_open_iso(path: str, size: int = 0, paused: bool = False) -> int:
+    cmd: list[str] = [ENV_MPVL_MPV_CMD]
+    if paused:
+        cmd.append(MPV_PAUSE_FLAG)
+    if size < DVD9_SIZE:
+        cmd.append("".join((MPV_DVD_ISO_OPTION, '"', path, '"')))
+    else:
+        cmd.append("".join((MPV_BD_ISO_OPTION, '"', path, '"')))
+    return exec_nonblocking(SPACE_CHAR.join(cmd))
+
+
+def process_exists(pid: int) -> bool:
+    return os.path.isdir(f"/proc/{pid}")
 
 
 def fixed_len_slice(data: Sequence, max_len: int, offset: int = 0) -> Sequence:
@@ -132,6 +158,10 @@ class Navi:
 
             full: str = os.path.join(self.get_current_dir(), item)
             item_stat: os.stat_result = os.stat(full)
+
+            if not item_stat.st_size:
+                continue
+
             self.fd_list.append(FdItem(is_dir=os.path.isdir(full), name=item, full=full, size=item_stat.st_size))
 
     def get_current_dir(self) -> str:
@@ -156,10 +186,7 @@ class Navi:
 
 
 class NaviFiltered(Navi):
-    """x"""
-
     def __init__(self, current_dir: str = ".") -> None:
-        """x"""
         Navi.__init__(self, current_dir)
 
         self.fd_list_filtered: list[FdItem] = []
@@ -170,37 +197,29 @@ class NaviFiltered(Navi):
         self.filter_text: str = ""
 
     def discard_fd_list_filtered(self) -> None:
-        """x"""
         self.fd_list_filtered *= 0
 
     def toggle_filter_show_dirs(self) -> bool:
-        """x"""
         self.filter_show_dirs = not self.filter_show_dirs
         return self.filter_show_dirs
 
     def toggle_filter_show_hidden(self) -> bool:
-        """x"""
         self.filter_show_hidden = not self.filter_show_hidden
         return self.filter_show_hidden
 
     def set_filter_exts(self, exts: tuple[str, ...]) -> None:
-        """x"""
         self.filter_exts = exts
 
     def set_filter_text(self, text: str) -> None:
-        """x"""
         self.filter_text = text
 
     def get_filter_text(self) -> str:
-        """x"""
         return self.filter_text
 
     def discard_filter_text(self) -> None:
-        """x"""
         self.filter_text *= 0
 
     def refresh_fd_list_filtered(self) -> None:
-        """x"""
         self.discard_fd_list_filtered()
 
         for item in self.fd_list:
@@ -216,7 +235,6 @@ class NaviFiltered(Navi):
             self.fd_list_filtered.append(item)
 
     def get_index_by_name(self, name: str) -> int:
-        """x"""
         i: int = -1
         for c, item in enumerate(self.fd_list_filtered):
             if name == item.name:
@@ -226,8 +244,6 @@ class NaviFiltered(Navi):
 
 
 class App:
-    """x"""
-
     UNUSED_LINES: int = 3
     LIST_START: int = 1
 
@@ -240,37 +256,40 @@ class App:
         curses.use_default_colors()
 
         curses.init_pair(1, -1, -1)  # default
-        # (1) Blue    -> directory
-        # (2) Yellow  -> audio
-        # (3) Green   -> video
-        # (4) Magenta -> subtitles
-        # (5) Cyan    -> last_item
+        # (2) Green   -> video
+        # (3) Yellow  -> audio
+        # (4) Blue    -> directory
+        # (5) Magenta -> subtitles
+        # (6) Cyan    -> 'last_item'
+        # (7) Red     -> other
         try:
-            curses.init_pair(2, 12, -1)
-            curses.init_pair(3, 11, -1)
-            curses.init_pair(4, 10, -1)
-            curses.init_pair(5, 13, -1)
-            curses.init_pair(6, 14, -1)
+            curses.init_pair(2, 10, -1)  # fg: (intense) green
+            curses.init_pair(3, 11, -1)  # fg: (intense) yellow
+            curses.init_pair(4, 12, -1)  # fg: (intense) blue
+            curses.init_pair(5, 13, -1)  # fg: (intense) magenta
+            curses.init_pair(6, 14, -1)  # fg: (intense) cyan
+            curses.init_pair(7, 15, -1)  # fg: (intense) red
         except ValueError:
-            curses.init_pair(2, 4, -1)
-            curses.init_pair(3, 3, -1)
-            curses.init_pair(4, 2, -1)
-            curses.init_pair(5, 5, -1)
-            curses.init_pair(6, 6, -1)
+            curses.init_pair(2, 2, -1)  # fg: green
+            curses.init_pair(3, 3, -1)  # fg: yellow
+            curses.init_pair(4, 4, -1)  # fg: blue
+            curses.init_pair(5, 5, -1)  # fg: magenta
+            curses.init_pair(6, 6, -1)  # fg: cyan
+            curses.init_pair(7, 1, -1)  # fg: red
 
         self.refresh_sizes()
 
         self.cursor: int = 0
         self.offset: int = 0
 
-        self.last_item_file = last_item_file
+        self.last_item_file: str = last_item_file
         self.last_item: str = ""
         try:
-            self.last_item = Path(self.last_item_file).read_text().strip()
+            self.last_item = Path(self.last_item_file).read_text(encoding="utf-8").strip()
         except FileNotFoundError:
             pass
 
-        self.navi_filtered = NaviFiltered()
+        self.navi_filtered: NaviFiltered = NaviFiltered()
         self.navi_filtered.set_filter_exts(MPV_EXTENSIONS)
         if not self.restore_last_item():
             self.navi_filtered.refresh_fd_list()
@@ -290,7 +309,7 @@ class App:
         return False
 
     def save_last_item(self) -> None:
-        Path(self.last_item_file).write_text(self.last_item)
+        Path(self.last_item_file).write_text(self.last_item, encoding="utf-8")
 
     def refresh_sizes(self) -> None:
         curses.update_lines_cols()
@@ -317,7 +336,7 @@ class App:
             status_line.append("[SUB]")
         pids_num: int = len(self.pids)
         if pids_num:
-            status_line.append("[mpv:{}]".format(str(pids_num)))
+            status_line.append(f"[mpv:{pids_num}]")
         if not self.navi_filtered.filter_show_dirs:
             status_line.append("(NO DIR.)")
         if self.navi_filtered.filter_show_hidden:
@@ -333,15 +352,17 @@ class App:
 
             if item.is_dir:
                 name = "".join((name, "/"))
-                style = curses.color_pair(2)
+                style = curses.color_pair(4)
             elif item.full == self.last_item:
                 style = curses.color_pair(6)
+            elif ext in VIDEO_EXTS:
+                style = curses.color_pair(2)
             elif ext in AUDIO_EXTS:
                 style = curses.color_pair(3)
-            elif ext in VIDEO_EXTS:
-                style = curses.color_pair(4)
             elif ext in SUB_EXTS:
                 style = curses.color_pair(5)
+            elif ext in OTHER_EXTS:
+                style = curses.color_pair(7)
             else:
                 pass
 
@@ -353,7 +374,10 @@ class App:
             line_num += 1
 
         # Message line:
-        self.window.addstr(curses.LINES - 2, 0, str(fixed_len_slice(self.current_message, curses.COLS)))
+        if self.current_message:
+            self.window.addstr(
+                curses.LINES - 2, 0, str(fixed_len_slice(SPACE_CHAR.join(("[LM]", self.current_message)), curses.COLS))
+            )
 
         # Name filter input:
         try:
@@ -367,7 +391,7 @@ class App:
 
         self.window.refresh()
 
-    def choose(self, with_save: bool = False) -> None:
+    def choose(self, with_save: bool = False, mpv_paused: bool = False) -> None:
         try:
             item = self.get_cursor_item()
         except IndexError:  # A typical case when the list is empty.
@@ -384,21 +408,21 @@ class App:
             if ext in SUB_EXTS:
                 if self.sub_path != item.full:
                     self.sub_path = item.full
-                    self.current_message = "Subtitle selected: {}".format(item.name)
+                    self.current_message = f"Subtitle selected: {item.name}"
                 else:
                     self.sub_path *= 0
                     self.current_message = "No subtitle selected."
                 return
 
             if ext == "iso":  # Requires a different command.
-                return
-
-            self.pids.append(mpv_open(item.full, self.sub_path))
+                self.pids.append(mpv_open_iso(item.full, item.size, mpv_paused))
+            else:
+                self.pids.append(mpv_open(item.full, self.sub_path, mpv_paused))
 
             if with_save:
                 self.last_item = item.full
                 self.save_last_item()
-                self.current_message = "Saved for later playback: {}".format(item.name)
+                self.current_message = f"Saved for later playback: {item.name}"
 
     def input_down(self) -> None:
         if self.get_index() == len(self.navi_filtered.fd_list_filtered) - 1:
@@ -463,8 +487,13 @@ class App:
         elif wch == curses.KEY_RIGHT:
             self.choose()
         elif wch in (curses.KEY_ENTER, "\n"):
-            self.choose(with_save=True)
+            self.choose(with_save=True, mpv_paused=True)
+        elif wch in (curses.KEY_HOME, curses.KEY_PPAGE):
+            self.reset_index()
+        elif wch in (curses.KEY_END, curses.KEY_NPAGE):
+            self.set_cursor(len(self.navi_filtered.fd_list_filtered) - 1)
         elif wch == curses.KEY_F5:
+            self.check_pids()
             self.current_message *= 0
             self.navi_filtered.refresh_fd_list()
             self.navi_filtered.refresh_fd_list_filtered()
@@ -480,12 +509,13 @@ class App:
         elif wch == curses.KEY_F8:
             self.restore_last_item()
         elif wch == curses.KEY_F9:
-            pass
+            self.pids.append(mpv_open(self.navi_filtered.get_current_dir()))
+            self.current_message = "Entire directory selected for playback. (after F9)"
         elif wch == curses.KEY_F10:
             self.sub_path *= 0
-            self.current_message = "[F10] No subtitle selected."
+            self.current_message = "No subtitle selected. (after F10)"
         elif wch == curses.KEY_F11:
-            pass
+            self.close_last_mpv()
         elif wch == curses.KEY_F12:  # exit
             return True
         elif wch == curses.KEY_BACKSPACE:
@@ -503,6 +533,24 @@ class App:
                 self.reset_index()
 
         return False
+
+    def check_pids(self) -> None:
+        self.pids = list(filter(process_exists, self.pids))
+
+    def close_last_mpv(self) -> None:
+        self.check_pids()
+        if not self.pids:
+            self.current_message = "mpv instance is not running."
+            return
+
+        last_pid: int = self.pids.pop()
+
+        try:
+            os.kill(last_pid, SIGTERM)
+        except ProcessLookupError:
+            pass
+        else:
+            self.current_message = f"mpv closed. (PID: {last_pid})"
 
     def loop(self) -> None:
         while 1:
@@ -524,6 +572,10 @@ WORK_DIR: str = os.path.join(os.path.expanduser("~"), ".local", "state", "mpl")
 LAST_ITEM_FILE: str = os.path.join(WORK_DIR, "last_item")
 
 if __name__ == "__main__":
+    if not os.getuid():
+        print("Please do not run the program with administrative privileges!")
+        sys.exit()
+
     if not os.path.exists(WORK_DIR):
         os.makedirs(WORK_DIR, 0o744)
 
